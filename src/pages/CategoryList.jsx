5ed2196eb409c3377a0a245183c29c558d06908c
SoulGiannis <chaudharyrishabh029@gmail.com>
2024-04-14 00:05:42 +0530

    added category list DSA

--- src/pages/CategoryList.jsx
+++ src/pages/CategoryList.jsx
@@ -4,10 +4,121 @@ import { NavLink } from 'react-router-dom'
 const CategoryList = () => {
   return (
     <>
-    <div>CategoryList</div>
-    <NavLink to="/dsa/categorylist/solution">solution</NavLink>
+    <div><CategoryListSub/></div>
+      <NavLink to="/dsa/categorylist/solution">solution</NavLink>
     </>
   )
 }
+
+const people = [
+  {
+    name: ' ',
+    email: ' ',
+    role: ' ',
+    imageUrl:
+      '',
+    lastSeen: ' ',
+    lastSeenDateTime: ' ',
+  },
+  {
+    name: ' ',
+    email: ' ',
+    role: ' ',
+    imageUrl:
+      '',
+    lastSeen: ' ',
+    lastSeenDateTime: ' ',
+  },
+  {
+    name: 'normal',
+    email: 'checkout ->',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Dries Vincent',
+    email: 'Check Code ->',
+    role: 'Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+  {
+    name: 'Lindsay Walton',
+    email: 'Check Code ->',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Courtney Henry',
+    email: 'Check Code ->',
+    role: 'Designer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Tom Cook',
+    email: 'check out ->',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+  {
+    name: 'Tom Cook',
+    email: 'check out ->',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+  {
+    name: 'Tom Cook',
+    email: 'check out ->',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+  {
+    name: 'Tom Cook',
+    email: 'check out ->',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+]
+
+function CategoryListSub() {
+  return (
+    <div className='flex justify-center bg-black'>
+      <ul role="list" className="divide-y bg-black divide-black">
+        {people.map((person) => (
+          <li key={person.email} className="flex justify-between gap-x-6 py-5">
+            <div className="flex min-w-0 gap-x-4 cursor-pointer">
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-white">{person.name}</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+              </div>
+            </div>
+            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-white">{person.role}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
 
 export default CategoryList
